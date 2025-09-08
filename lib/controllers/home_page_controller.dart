2ae4ac0de4c1954c75b4353b5c27089e02751e9c
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-09-08 16:58:34 +0600

    Added Homepagecontroller StateNotifierProvider

---
+++ lib/controllers/home_page_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:poke_buddy/models/page_data.dart';
+
+class HomePageController extends StateNotifier<HomePageData> {
+  HomePageController(super.state);
+
+}
